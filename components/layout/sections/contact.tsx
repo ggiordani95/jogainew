4249a890b557a226375683665f62deb901a7658e
Gustavo Giordani <gustavogiordani@192.168.0.92>
2024-10-27 17:00:15 -0300

    improving form

--- components/layout/sections/contact.tsx
+++ components/layout/sections/contact.tsx
@@ -1,80 +1,78 @@
 "use client";
 import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
 } from "@/components/ui/card";
 import { Building2, Clock, Mail, Phone } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 
 const formSchema = z.object({
-  firstName: z.string().min(2).max(255),
-  lastName: z.string().min(2).max(255),
-  email: z.string().email(),
-  subject: z.string().min(2).max(255),
-  message: z.string(),
+	firstName: z.string().min(2).max(255),
+	lastName: z.string().min(2).max(255),
+	email: z.string().email(),
+	subject: z.string().min(2).max(255),
+	message: z.string(),
 });
 
 export const ContactSection = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      subject: "Web Development",
-      message: "",
-    },
-  });
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			firstName: "",
+			lastName: "",
+			email: "",
+			subject: "Web Development",
+			message: "",
+		},
+	});
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    const { firstName, lastName, email, subject, message } = values;
-    console.log(values);
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		const { firstName, lastName, email, subject, message } = values;
+		console.log(values);
+	}
 
-    const mailToLink = `mailto:leomirandadev@gmail.com?subject=${subject}&body=Hello I am ${firstName} ${lastName}, my Email is ${email}. %0D%0A${message}`;
+	return (
+		<section id="contact" className="container py-24 sm:py-32">
+			<section className="grid grid-cols-1 md:grid-cols-2 gap-8">
+				<div>
+					<div className="mb-4">
+						<h2 className="text-lg text-primary mb-2 tracking-wider">
+							Contato
+						</h2>
 
-    window.location.href = mailToLink;
-  }
+						<h2 className="text-3xl md:text-4xl font-bold">
+							Entre em contato com a gente
+						</h2>
+					</div>
+					<p className="mb-8 text-muted-foreground lg:w-5/6">
+						Envie mensagem pelo nosso site ou por nosso e-mail. Iremos responder
+						em até 1 (um) dia útil.
+					</p>
 
-  return (
-    <section id="contact" className="container py-24 sm:py-32">
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div>
-          <div className="mb-4">
-            <h2 className="text-lg text-primary mb-2 tracking-wider">
-              Contato
-            </h2>
-
-            <h2 className="text-3xl md:text-4xl font-bold">Entre em contato com a gente</h2>
-          </div>
-          <p className="mb-8 text-muted-foreground lg:w-5/6">
-            Envie mensagem pelo nosso site ou por nosso e-mail. Iremos responder em até 1 (um) dia útil.
-          </p>
-
-          <div className="flex flex-col gap-4">
-
-            {/* <div>
+					<div className="flex flex-col gap-4">
+						{/* <div>
               <div className="flex gap-2 mb-1">
                 <Phone />
                 <div className="font-bold">Call us</div>
@@ -83,154 +81,148 @@ export const ContactSection = () => {
               <div>+1 (619) 123-4567</div>
             </div> */}
 
-            <div>
-              <div className="flex gap-2 mb-1">
-                <Mail />
-                <div className="font-bold">Nosso e-mail</div>
-              </div>
+						<div>
+							<div className="flex gap-2 mb-1">
+								<div className="text-primary font-bold">Nosso e-mail</div>
+							</div>
 
-              <div>jogaiapp@gmail.com</div>
-            </div>
+							<div className="mt-3 text-white/90 font-normal tracking-wider">
+								jogaiapp@gmail.com
+							</div>
+						</div>
 
-            <div>
-              <div className="flex gap-2">
-                <Clock />
-                <div className="font-bold">Horário de Atendimento Comercial</div>
-              </div>
+						<div className="mt-3">
+							<div className="flex gap-2">
+								<div className=" text-primary font-bold">
+									Horário de Atendimento
+								</div>
+							</div>
 
-              <div>
-                <div>Segunda - Sexta</div>
-                <div>9:00 - 17:00</div>
-              </div>
-            </div>
-          </div>
-        </div>
+							<div className="mt-3 text-white/90 font-normal tracking-wider gap-4">
+								<div>Segunda - Sexta</div>
+								<div>9:00 - 17:00</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        <Card className="bg-muted/60 dark:bg-card">
-          <CardHeader className="text-primary text-2xl"> </CardHeader>
-          <CardContent>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="grid w-full gap-4"
-              >
-                <div className="flex flex-col md:!flex-row gap-8">
-                  <FormField
-                    control={form.control}
-                    name="firstName"
-                    render={({ field }) => (
-                      <FormItem className="w-full">
-                        <FormLabel>Primeiro nome</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Nome" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="lastName"
-                    render={({ field }) => (
-                      <FormItem className="w-full">
-                        <FormLabel>Sobrenome</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Sobrenome" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+				<Card className="bg-muted/60 dark:bg-card">
+					<CardHeader className="text-primary text-2xl"> </CardHeader>
+					<CardContent>
+						<Form {...form}>
+							<form
+								onSubmit={form.handleSubmit(onSubmit)}
+								className="grid w-full gap-4"
+							>
+								<div className="flex flex-col md:!flex-row gap-8">
+									<FormField
+										control={form.control}
+										name="firstName"
+										render={({ field }) => (
+											<FormItem className="w-full">
+												<FormLabel>Primeiro nome</FormLabel>
+												<FormControl>
+													<Input placeholder="Nome" {...field} />
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name="lastName"
+										render={({ field }) => (
+											<FormItem className="w-full">
+												<FormLabel>Sobrenome</FormLabel>
+												<FormControl>
+													<Input placeholder="Sobrenome" {...field} />
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</div>
 
-                <div className="flex flex-col gap-1.5">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>E-mail</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="email"
-                            placeholder="seuemail@email.com"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+								<div className="flex flex-col gap-1.5">
+									<FormField
+										control={form.control}
+										name="email"
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>E-mail</FormLabel>
+												<FormControl>
+													<Input
+														type="email"
+														placeholder="seuemail@email.com"
+														{...field}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</div>
 
-                <div className="flex flex-col gap-1.5">
-                  <FormField
-                    control={form.control}
-                    name="subject"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Assunto</FormLabel>
-                        <Select
-                          onValueChange={field.onChange}
-                          defaultValue={field.value}
-                        >
-                          <FormControl>
-                            <SelectTrigger>
-                              <SelectValue placeholder="Select a subject" />
-                            </SelectTrigger>
-                          </FormControl>
-                          <SelectContent>
-                            <SelectItem value="Web Development">
-                              Planos
-                            </SelectItem>
-                            <SelectItem value="Mobile Development">
-                              Financeiro
-                            </SelectItem>
-                            <SelectItem value="Figma Design">
-                              Figma Design
-                            </SelectItem>
-                            <SelectItem value="REST API">REST API</SelectItem>
-                            <SelectItem value="FullStack Project">
-                              Outro
-                            </SelectItem>
-                          </SelectContent>
-                        </Select>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
+								<div className="flex flex-col gap-1.5">
+									<FormField
+										control={form.control}
+										name="subject"
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Assunto</FormLabel>
+												<Select
+													onValueChange={field.onChange}
+													defaultValue={field.value}
+												>
+													<FormControl>
+														<SelectTrigger>
+															<SelectValue placeholder="Select a subject" />
+														</SelectTrigger>
+													</FormControl>
+													<SelectContent>
+														<SelectItem value="Planos">Planos</SelectItem>
+														<SelectItem value="Sistema">
+															Dúvidas sobre o sistema
+														</SelectItem>
+														<SelectItem value="Contato">
+															Apenas contato
+														</SelectItem>
+														<SelectItem value="Outro">Outro</SelectItem>
+													</SelectContent>
+												</Select>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</div>
 
-                <div className="flex flex-col gap-1.5">
-                  <FormField
-                    control={form.control}
-                    name="message"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Mensagem</FormLabel>
-                        <FormControl>
-                          <Textarea
-                            rows={5}
-                            placeholder="Sua mensagem..."
-                            className="resize-none"
-                            {...field}
-                          />
-                        </FormControl>
+								<div className="flex flex-col gap-1.5">
+									<FormField
+										control={form.control}
+										name="message"
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Mensagem</FormLabel>
+												<FormControl>
+													<Textarea
+														rows={5}
+														placeholder="Sua mensagem..."
+														className="resize-none"
+														{...field}
+													/>
+												</FormControl>
 
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-
-                <Button className="mt-4 text-black">Enviar Mensagem</Button>
-              </form>
-            </Form>
-          </CardContent>
-
-          <CardFooter></CardFooter>
-        </Card>
-      </section>
-    </section>
-  );
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</div>
+								<Button className="mt-4 text-black">Enviar Mensagem</Button>
+							</form>
+						</Form>
+					</CardContent>
+				</Card>
+			</section>
+		</section>
+	);
 };
--- components/layout/sections/sponsors.tsx
+++ components/layout/sections/sponsors.tsx
@@ -7,67 +7,65 @@ import tennis from "../../../public/tennis.png";
 import soccer from "../../../public/soccer.png";
 import volley from "../../../public/volley.png";
 interface sponsorsProps {
-  icon: any;
-  name: string;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	icon: any;
+	name: string;
 }
 
 const sponsors: sponsorsProps[] = [
-  {
-    icon: <Image src={soccer} alt="soccer" width={120} height={122} />,
-    name: "Futebol Society",
-  },
-  {
-    icon: <Image src={tennis} alt="tennis" width={120} height={122} />,
-    name: "Beach Tennis",
-  },
-  {
-    icon: <Image src={soccer} alt="soccer" width={120} height={122} />,
-    name: "Futebol 11",
-  },
-  {
-    icon: <Image src={tennis} alt="tennis" width={120} height={122} />,
-    name: "Tênis",
-  },
-  {
-    icon: <Image src={volley} alt="volley" width={120} height={122} />,
-    name: "Futevôlei",
-  },
-  {
-    icon: <Image src={soccer} alt="soccer" width={120} height={122} />,
-    name: "Futebol de Areia",
-  },
-  
+	{
+		icon: <Image src={soccer} alt="soccer" width={120} height={122} />,
+		name: "Futebol Society",
+	},
+	{
+		icon: <Image src={tennis} alt="tennis" width={120} height={122} />,
+		name: "Beach Tennis",
+	},
+	{
+		icon: <Image src={soccer} alt="soccer" width={120} height={122} />,
+		name: "Futebol 11",
+	},
+	{
+		icon: <Image src={tennis} alt="tennis" width={120} height={122} />,
+		name: "Tênis",
+	},
+	{
+		icon: <Image src={volley} alt="volley" width={120} height={122} />,
+		name: "Futevôlei",
+	},
+	{
+		icon: <Image src={soccer} alt="soccer" width={120} height={122} />,
+		name: "Futebol de Areia",
+	},
 ];
 
 export const SponsorsSection = () => {
-  return (
-    <div className="relative ">
-      <section
-        id="sponsors"
-        className="max-w-[100%] mx-auto pb-32 pt-0"
-      >
-        {/* <h2 className=" duration-10000 text-white text-3xl font-semibold md:text-4xl text-center mb-14">
+	return (
+		<section
+			id="sponsors"
+			className="max-w-[100%] mx-auto pb-32 pt-0 relative  bg-transparent"
+		>
+			{/* <h2 className=" duration-10000 text-white text-3xl font-semibold md:text-4xl text-center mb-14">
           Confira os esportes disponíveis{" "}
         </h2> */}
-        <div className="mx-auto pt-6">
-          <Marquee
-            className="gap-[2.4rem]"
-            numberOfCopies={4}
-            fade
-            innerClassName="gap-[2.4rem]"
-          >
-            {sponsors.map(({ icon, name }) => (
-              <div
-                key={name}
-                className="flex items-center bg-card/20 text-lg md:text-3xl  text-card-foreground/50 p-6 rounded-2xl gap-3"
-              >
-                <div className="max-w-[42px] mr-2 ">{icon}</div>
-                <span>{name}</span>
-              </div>
-            ))}
-          </Marquee>
-        </div>
-      </section>
-    </div>
-  );
+			<div className="mx-auto pt-6">
+				<Marquee
+					className="gap-[2.4rem]"
+					numberOfCopies={4}
+					fade
+					innerClassName="gap-[2.4rem]"
+				>
+					{sponsors.map(({ icon, name }) => (
+						<div
+							key={name}
+							className="flex items-center bg-card/20 text-lg md:text-3xl  text-card-foreground/50 p-6 rounded-2xl gap-3"
+						>
+							<div className="max-w-[42px] mr-2 ">{icon}</div>
+							<span>{name}</span>
+						</div>
+					))}
+				</Marquee>
+			</div>
+		</section>
+	);
 };
